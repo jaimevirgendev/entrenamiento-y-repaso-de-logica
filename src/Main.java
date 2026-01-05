1a12c0f1bb3a5c7c12b19a2d6ce6318a5d2c6e8b
Jaimevirgendev <jaimeoreavirgen511@gmail.com>
2026-01-05 13:55:54 -0600

    Fix error

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,6 @@ import calentamiento.Factorial;
 
 public class Main {
     public static void main(String[] args) {
-        //esPar();
-        factorial();
-    }
 
-    private static void esPar(){
-        boolean isPar = EsPar.isPar();
-        if (!isPar){
-            System.out.println("No es par");
-        } else {
-            System.out.println("Es par");
-        }
-    }
-
-    private static void factorial(){
-        //int factorial = Factorial.factorialV2(5);
-        //System.out.println("El factorial es: " + factorial);
     }
 }
