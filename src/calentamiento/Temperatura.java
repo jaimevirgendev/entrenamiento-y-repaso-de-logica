22589a75a61212bb26e7dba424baec77d9cbcc37
Jaimevirgendev <jaimeoreavirgen511@gmail.com>
2026-01-06 15:53:37 -0600

    Ejercicio 5: Calculadora de temperatura.

--- src/calentamiento/Temperatura.java
+++ src/calentamiento/Temperatura.java
@@ -11,10 +11,6 @@ import java.util.Scanner;
 public class Temperatura {
     static Scanner read = new Scanner(System.in);
 
-    public static void main(String[] args){
-        mainMenu();
-    }
-
     private static void mainMenu(){
         int opc;
         do {
@@ -36,6 +32,7 @@ public class Temperatura {
                     break;
                 case 3:
                     menuKelvin();
+                    System.out.println("-------------------------");
                     break;
                 case 4:
                     System.out.println("Ing. Jaime Orea Virgen");
@@ -43,6 +40,7 @@ public class Temperatura {
                     break;
                 default:
                     System.out.println("Incorrect option, choose a correct option...");
+                    System.out.println("Press enter to return to the main menu...");
                     read.nextLine();
             }
         } while(opc != 4);
@@ -56,7 +54,7 @@ public class Temperatura {
             System.out.println("[1]: Celsius to Fahrenheit.");
             System.out.println("[2]: Celsius to Kelvin");
             System.out.println("[3]: Return to main menu");
-            System.out.print("Ingrese la opción correcta: ");
+            System.out.print("Choose the correct option: ");
             opc = Integer.parseInt(read.nextLine());
 
             switch(opc){
@@ -71,7 +69,7 @@ public class Temperatura {
                     break;
                 case 3:
                     System.out.println("Returning to the main menu...");
-                    System.out.print("Presiona enter para continuar...");
+                    System.out.print("Press enter to return to the main menu...");
                     read.nextLine();//Limpiar el buffer para el regreso
                     break;
                 default:
@@ -87,8 +85,8 @@ public class Temperatura {
             System.out.println("*** Menu de...");
             System.out.println("[1]: Fahrenheit a Celsius");
             System.out.println("[2]: Fahrenheit a Kelvin");
-            System.out.println("[3]: Volver al menu principal");
-            System.out.print("Ingrese la opción correcta: ");
+            System.out.println("[3]: Return to the main menu...");
+            System.out.print("Choose the correct option: ");
             opc = Integer.parseInt(read.nextLine());
             switch(opc){
                 case 1:
@@ -100,8 +98,8 @@ public class Temperatura {
                     System.out.printf("Fahrenheit to Kelvin: %f", resultado);
                     break;
                 case 3:
-                    System.out.println("Regresando al menu principal...");
-                    System.out.print("Presiona enter para continuar...");
+                    System.out.println("Returning to the main menu...");
+                    System.out.print("Press enter to return to the main menu...");
                     read.nextLine();//pausa
                     break;
                 default:
@@ -117,8 +115,8 @@ public class Temperatura {
             System.out.println("*** Menu...");
             System.out.println("[1]: Kelvin to Celsius");
             System.out.println("[2]: Kelvin to Fahrenheit");
-            System.out.println("[3]: Regresar al menu principal");
-            System.out.print("Ingrese la opcion deseada: ");
+            System.out.println("[3]: Return to the main menu...");
+            System.out.print("Choose the correct option: ");
             opc = Integer.parseInt(read.nextLine());
             switch(opc){
                 case 1:
@@ -130,8 +128,8 @@ public class Temperatura {
                     System.out.printf("Kelvin to Fahrenheit: %f", resultado);
                     break;
                 case 3:
-                    System.out.println("Regresando al menu principal...");
-                    System.out.print("Press enter to return to the main menu..");
+                    System.out.println("Returning to the main menu...");
+                    System.out.print("Press enter to return to the main menu...");
                     break;
                 default:
                     System.out.println("Incorrect option, choose a correct option...");
@@ -141,17 +139,17 @@ public class Temperatura {
     }
 
     private static int celsiusFahrenheit(){
-        int fahren, temperatura;
-        System.out.print("Ingrese la temperatura en celsius: ");
+        int fahrenheit, temperatura;
+        System.out.print("Enter the temperature in Celsius: ");
         temperatura = Integer.parseInt(read.nextLine());
-        fahren = (temperatura * 9/5) + 32;
-        return fahren;
+        fahrenheit = (temperatura * 9/5) + 32;
+        return fahrenheit;
     }
 
     private static double celsiusKelvin() {
         int temperatura;
         double kelvin;
-        System.out.print("Ingrese la temperatura en celsius: ");
+        System.out.print("Enter the temperature in Celsius: ");
         temperatura = Integer.parseInt(read.nextLine());
         kelvin = temperatura + 273.15;
         return kelvin;
@@ -160,7 +158,7 @@ public class Temperatura {
     private static double fahrenheitCelsius(){
         double celsius;
         int temperatura;
-        System.out.print("Ingrese la temperatura en Fahrenheit: ");
+        System.out.print("Enter the temperature in Fahrenheit: ");
         temperatura = Integer.parseInt(read.nextLine());
         celsius = (double)(temperatura - 32) * 5 / 9;
         return celsius;
@@ -168,7 +166,7 @@ public class Temperatura {
 
     private static double fahrenheitKelvin(){
         double kelvin, temperatura;
-        System.out.print("Ingrese los grados en Fahrenheit: ");
+        System.out.print("Enter the temperature in Fahrenheit: ");
         temperatura = Double.parseDouble(read.nextLine());
         kelvin = (temperatura - 32) * 5 / 9 + 273.15;
         return kelvin;
@@ -176,7 +174,7 @@ public class Temperatura {
 
     private static double kelvinCelsius() {
         double celsius, temperatura;
-        System.out.print("Ingrese los grados Kelvin: ");
+        System.out.print("Enter the temperature in Kelvin: ");
         temperatura = Double.parseDouble(read.nextLine());
         celsius = temperatura -273.15;
         return celsius;
@@ -184,7 +182,7 @@ public class Temperatura {
 
     private static double kelvinFahrenheit() {
         double temperatura, fahrenheit;
-        System.out.print("Ingrese los grados en Fahrenheit: ");
+        System.out.print("Enter the temperature in Fahrenheit: ");
         temperatura = Double.parseDouble(read.nextLine());
         fahrenheit = (temperatura - 273.15) * 9 / 5 + 32;
         return fahrenheit;
